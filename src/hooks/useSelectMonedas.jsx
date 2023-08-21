aba5f6a00e8c8d86a5d4c707ba30b770af01823c
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-21 12:02:45 -0300

    Creando un Custom Hook

---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,12 @@
+//import React from 'react'
+
+const useSelectMonedas = () => {
+  // Un hook retorna un objeto o un arreglo
+
+  const SelectMonedas = () => {
+    console.log('Desde Select Monedas')
+  }
+  return [ SelectMonedas ]
+}
+
+export default useSelectMonedas
