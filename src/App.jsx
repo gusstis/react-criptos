fc5f2653b302568131d07bb8ca0cb5eecab0a1eb
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-21 17:48:08 -0300

    Detectando los valores de las monedas con useEffect()

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-//import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import Formulario from './Components/Formulario'
 import ImagenCripto from './img/imagen-criptos.png'
@@ -41,6 +41,15 @@ const Heading = styled.h1 `
 
 function App() {
 
+  const [monedas, setMonedas] = useState({})
+  useEffect(() => {
+    if(Object.keys(monedas).length > 0) {
+      console.log(monedas)
+    }  
+  
+  }, [monedas])
+  
+
   return (
     <Contenedor>
       <Imagen
@@ -50,7 +59,9 @@ function App() {
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
 
-        <Formulario></Formulario>
+        <Formulario
+          setMonedas={setMonedas}
+        />
       </div>
     </Contenedor>
   )
