5e786124c2b7d63928f276d329cc8f9c06f2a274
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-21 11:00:31 -0300

    Creación del Input del Formulario

---
+++ src/Components/Formulario.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const InputSubmit = styled.input`
+    background-color: #9497ff;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #fff;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s;
+
+    &:hover {
+      background-color: #7a7efd;
+      cursor: pointer;
+    }
+`
+
+const Formulario = () => {
+  return (
+    <form>
+
+        <InputSubmit
+            type="submit"
+            value='cotizar'
+        />
+    </form>
+  )
+}
+
+export default Formulario
